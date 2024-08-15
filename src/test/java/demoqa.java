40226edaab4832a3c1049f6931434d666fba2572
Docen89 <docen89@ya.ru>
2024-08-15 08:44:39 +0500

    v2

--- src/test/java/demoqa.java
+++ src/test/java/demoqa.java
@@ -1,35 +1,71 @@
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class demoqa {
 
-  public static void main(String[] args) {
-    // Создаем экземпляр WebDriver
-    WebDriver driver = new ChromeDriver();
-    // Открываем сайт, используя драйвер
+  WebDriver driver;
+
+  @BeforeEach
+  public void setup() {
+    driver = new ChromeDriver();
     driver.get("http://demoqa.com/");
-    // Находим элемент по xpath
-    WebElement element = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]"));
-    // кликаем по нему
+    driver.manage().window().maximize();
+  }
+
+  @Test
+  void TestDemoQa_1() {
+    WebElement element = driver.findElement(
+        By.xpath("//div[@class='card mt-4 top-card']"));
     element.click();
-    WebElement element_text = driver.findElement(By.xpath("//*[@id=\"item-0\"]"));
-    element_text.click();
-    WebElement element_Name = driver.findElement(By.xpath("//*[@id=\"userName\"]"));
-    element_Name.sendKeys("Taras");
-    WebElement element_Email = driver.findElement(By.xpath("//*[@id=\"userEmail\"]"));
-    element_Email.sendKeys("test@bk.ru");
-    WebElement element_Cad = driver.findElement(By.xpath("//*[@id=\"currentAddress\"]"));
-    element_Cad.sendKeys("mars");
-    WebElement element_Pad = driver.findElement(By.xpath("//*[@id=\"permanentAddress\"]"));
-    element_Pad.sendKeys("forest");
-    WebElement element_Sub = driver.findElement(By.xpath("//*[@id=\"submit\"]"));
-    element_Sub.click();
+    WebElement elementText = driver.findElement(
+        By.xpath("//li[@id='item-0']"));
+    elementText.click();
+    WebElement elementName = driver.findElement(
+        By.xpath("//input[@id='userName']"));
+    elementName.sendKeys("Taras");
+    WebElement elementEmail = driver.findElement(
+        By.xpath("//input[@id='userEmail']"));
+    elementEmail.sendKeys("test@bk.ru");
+    WebElement elementCad = driver.findElement(
+        By.xpath("//textarea[@id='currentAddress']"));
+    elementCad.sendKeys("mars");
+    WebElement elementPad = driver.findElement
+    By.xpath("//textarea[@id='permanentAddress']"));
+    elementPad.sendKeys("forest");
+    WebElement elementSub = driver.findElement(
+        By.xpath("//button[@id='submit']"));
+    elementSub.click();
     assertEquals("https://demoqa.com/text-box", driver.getCurrentUrl());
+  }
+
+  @Test
+  void TestDemoQa_2() {
+    WebElement elementAlerts = driver.findElement(
+        By.xpath("//div[@class='category-cards']/div[3]"));
+    elementAlerts.click();
+    WebElement elementBrowserWindows = driver.findElement(
+        By.xpath("//li[@class='btn btn-light active']"));
+    elementBrowserWindows.click();
+    WebElement elementNewTab = driver.findElement(
+        By.xpath("//button[@id='tabButton']"));
+    elementNewTab.click();
+    Object[] windowHandles = driver.getWindowHandles().toArray();
+    driver.switchTo().window((String) windowHandles[1]);
+    String title = driver.getTitle();
+    assertEquals("https://demoqa.com/sample", driver.getCurrentUrl());
+  }
+
+  @AfterEach
+  public void tearDown() {
     driver.quit();
   }
 }
--- src/test/java/demoqa_browser_windows.java
+++ src/test/java/demoqa_browser_windows.java
@@ -1,25 +1,30 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demoqa_browser_windows {
-  public static void main(String[] args) {
-    WebDriver driver=new ChromeDriver();
-    driver.get("http://demoqa.com/");
-    WebElement element_Alerts= driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[3]"));
-    element_Alerts.click();
-    WebElement element_Browser_Windows=driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div/div/div[3]/div/ul/li[1]"));
-    element_Browser_Windows.click();
-    WebElement element_New_Tab= driver.findElement(By.xpath("//*[@id=\"tabButton\"]"));
-    element_New_Tab.click();
-    Object[] windowHandles=driver.getWindowHandles().toArray();
-    driver.switchTo().window((String) windowHandles[1]);
-    //assert on title of new window
-    String title=driver.getTitle();
-    assertEquals("https://demoqa.com/sample",driver.getCurrentUrl());
-    driver.quit();
-  }
+
+ @Test
+     void TestDemoQa_2() {
+   WebDriver driver = new ChromeDriver();
+   driver.get("http://demoqa.com/");
+   WebElement element_Alerts = driver.findElement(
+       By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[3]"));
+   element_Alerts.click();
+   WebElement element_Browser_Windows = driver.findElement(
+       By.xpath("/html/body/div[2]/div/div/div/div[1]/div/div/div[3]/div/ul/li[1]"));
+   element_Browser_Windows.click();
+   WebElement element_New_Tab = driver.findElement(By.xpath("//*[@id=\"tabButton\"]"));
+   element_New_Tab.click();
+   Object[] windowHandles = driver.getWindowHandles().toArray();
+   driver.switchTo().window((String) windowHandles[1]);
+   //assert on title of new window
+   String title = driver.getTitle();
+   assertEquals("https://demoqa.com/sample", driver.getCurrentUrl());
+   driver.quit();
+ }
 }
